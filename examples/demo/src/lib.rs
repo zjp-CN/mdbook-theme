20aa513af6c3eebf6be46f4b0a99adbfe8bc8ddf
zjp <jiping_zhou@foxmail.com>
2021-06-16 16:44:17 +0800

    fix demo && add workspace

--- examples/demo/src/lib.rs
+++ examples/demo/src/lib.rs
@@ -13,13 +13,13 @@ pub struct Nop;
 // }
 
 impl Preprocessor for Nop {
-    fn name(&self) -> &str { "theme-pre" }
+    fn name(&self) -> &str { "demo-pre" }
 
     fn run(&self, ctx: &PreprocessorContext, book: Book) -> Result<Book, Error> {
         if let Some(nop_cfg) = ctx.config.get_preprocessor(self.name()) {
             if nop_cfg.contains_key("blow-up") {
                 // anyhow::bail!("Boom!!!");
-                eprintln!("Boom!!!");
+                eprintln!("Boom!!!From preprocessor...");
             }
         }
 
