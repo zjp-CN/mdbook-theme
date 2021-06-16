4d702d2eb2c10816f5fa8f3e2d80c9debcabc000
zjp <jiping_zhou@foxmail.com>
2021-06-17 01:03:54 +0800

    theme-pre data structures

---
+++ pre/src/lib.rs
@@ -0,0 +1,26 @@
+use mdbook::book::Book;
+use mdbook::errors::Error;
+use mdbook::preprocess::{Preprocessor, PreprocessorContext};
+
+pub struct Nop;
+
+// impl Nop {
+//     pub fn new() -> Nop { Nop }
+// }
+
+impl Preprocessor for Nop {
+    fn name(&self) -> &str { "theme-pre" }
+
+    fn run(&self, ctx: &PreprocessorContext, book: Book) -> Result<Book, Error> {
+        if let Some(nop_cfg) = ctx.config.get_preprocessor(self.name()) {
+            if nop_cfg.contains_key("blow-up") {
+                // anyhow::bail!("Boom!!!");
+                eprintln!("Boom!!!From preprocessor...");
+            }
+        }
+
+        Ok(book)
+    }
+
+    fn supports_renderer(&self, renderer: &str) -> bool { renderer != "not-supported" }
+}
