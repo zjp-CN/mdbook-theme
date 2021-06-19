9b8c392e2d34f7c9f67e35b1cc7c702df76d2a5f
zjp <jiping_zhou@foxmail.com>
2021-06-20 01:26:45 +0800

    theme-pre parse input done

--- pre/src/theme/config.rs
+++ pre/src/theme/config.rs
@@ -1,4 +1,4 @@
-use super::{CssFile, Item, Ready, Theme, Value, DEFAULT_HASHMAP};
+use super::{CssFile, CssFile::Invalid, Item, Ready, Theme, Value, DEFAULT};
 use crate::error::{Error, Result};
 use std::collections::HashMap;
 use toml::map::Map;
@@ -17,24 +17,29 @@ pub fn process<'a>(input: &'a Map<String, value::Value>) {
 
     Theme::create_theme_dirs(); // create all dirs just once
 
+    let mut input = input.to_owned();
     if input.remove("pagetoc") // set all pagetoc related defaults
             .map_or(false, |p| p.as_bool().unwrap_or(false))
     {
-        Theme::from(CssFile::Pagetoc, Ready::default()).pagetoc();
+        Theme::from(CssFile::Pagetoc, Ready::default()).pagetoc(); // process pagetoc defaults
     }
 
-    let mut config = HashMap::new();
+    let default_map: HashMap<Item<'a>, CssFile> =
+        DEFAULT.iter().map(|(css, item, _)| (*item, *css)).collect();
+    let mut config = HashMap::new(); // ultimate theme configs
 
     dbg!(&input);
 
-    input.into_iter()
+    input.iter()
          .map(|(item, value)| {
-             (*config.entry(DEFAULT_HASHMAP.get(item).unwrap()).or_insert_with(Vec::new))
-             .push((Item(item.as_str()),Value(value.as_str().unwrap())))
+             let item = item.as_str();
+             (*config.entry(default_map.get(item).unwrap_or(&Invalid)).or_insert_with(Vec::new))
+             .push((Item(item),Value(value.as_str().unwrap())))
          })
          .last();
 
     config.into_iter()
+          .filter(|(css, _)| **css != Invalid) // exlude use's invalid configs
           .map(|(css, ready)| Theme::from(*css, Ready(ready)).process())
           .last();
 }
