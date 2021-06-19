866bde7412e0211e7d392faa5f8b43177b48f738
zjp <jiping_zhou@foxmail.com>
2021-06-19 20:54:52 +0800

    theme-pre support custom files done

--- pre/src/theme/mod.rs
+++ pre/src/theme/mod.rs
@@ -1,9 +1,10 @@
 use mdbook::theme::*;
 use std::borrow::Borrow;
 use std::collections::HashMap;
+use std::fmt;
 use std::hash::Hash;
 use std::iter::FromIterator;
-use std::str;
+use std::path::PathBuf;
 
 use crate::error::{Error, Result};
 
@@ -13,12 +14,13 @@ pub mod config;
 /// `Pagetoc` is not only single file
 #[rustfmt::skip]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum CssFile { Variables, General, Chrome, Index, PagetocJs, PagetocCss, Custom, Pagetoc, }
+pub enum CssFile { Variables, General, Chrome, Index, PagetocJs, PagetocCss, Pagetoc, Custom(&'static str) }
 
 impl CssFile {
-    pub fn filename(&self) -> &str {
+    pub fn filename(&self) -> &'static str {
         match self {
             CssFile::Variables => "css/variables.css",
+            CssFile::Custom(filename) => filename,
             _ => "temp.css",
         }
     }
@@ -45,8 +47,8 @@ impl CssFile {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct Item(&'static str);
 
-/// useful when looking up in `HashMap<&Item, _>`
-impl Borrow<str> for &Item {
+/// useful when looking up in `HashMap<&Item, _>` just via `HashMap<&str, _>`
+impl Borrow<str> for Item {
     fn borrow(&self) -> &str { self.0 }
 }
 
@@ -75,7 +77,7 @@ impl FromIterator<(Item, Value)> for Ready {
     fn from_iter<I: IntoIterator<Item = (Item, Value)>>(iter: I) -> Self {
         let mut r = Self::default();
         for i in iter {
-            r.add(i);
+            r.0.push(i);
         }
         r
     }
@@ -84,27 +86,37 @@ impl FromIterator<(Item, Value)> for Ready {
 /// yield default config or merge configs
 impl Ready {
     /// TODO: add more default config
-    fn get(css: CssFile) -> Self {
+    /// to get a default config from a specific cssfile
+    pub fn get_defualt(css: CssFile) -> Self {
         match css {
             c @ CssFile::Variables => Ready::from(c),
             _ => Self::default(),
         }
     }
 
+    /// help to simplify `.get()`
     fn from(css: CssFile) -> Self {
         DEFAULT.iter().filter(|(c, _, _)| *c == css).map(|(_, i, v)| (*i, *v)).collect()
     }
 
-    fn add(&mut self, elem: (Item, Value)) { self.0.push(elem); }
+    // fn add(&mut self, elem: (Item, Value)) { self.0.push(elem); }
 
     pub fn item_value(&self) -> &Vec<(Item, Value)> { &self.0 }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct Content(String);
 
 impl Default for Content {
     fn default() -> Self { Self(String::from("")) }
+}
+
+impl fmt::Display for Content {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { write!(f, "{}", self.0) }
+}
+
+impl fmt::Debug for Content {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { write!(f, "Content(...)") }
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -113,10 +125,20 @@ struct Pos(usize, usize);
 impl Content {
     /// TODO: add more contents
     pub fn from(cssfile: CssFile) -> Self {
+        use std::str;
         match cssfile {
             CssFile::Variables => Content(String::from(str::from_utf8(VARIABLES_CSS).unwrap())),
+            CssFile::Custom(filename) => Content::from_file(filename),
             _ => Content::default(),
         }
+    }
+
+    fn from_file(filename: &str) -> Self {
+        use std::io::Read;
+        let mut s = String::new();
+        let path = format!("theme/{}", filename);
+        std::fs::File::open(path).unwrap().read_to_string(&mut s).unwrap();
+        Content(s)
     }
 
     /// for viewing the content
@@ -131,7 +153,7 @@ impl Content {
         let text = self.get();
         let p1 = text.find(pat).ok_or(Error::StrNotFound)? + pat.len() + 2;
         let p2 = p1 + text[p1..].find(';').ok_or(Error::StrNotFound)?;
-        dbg!(&text[p1..p2]);
+        // dbg!(&text[p1..p2]);
         Ok(Pos(p1, p2))
     }
 
@@ -139,7 +161,8 @@ impl Content {
     fn replace(&mut self, pat: &str, sub: &str) -> Result<()> {
         let Pos(p1, p2) = self.find(pat)?;
         self.get_mut().replace_range(p1..p2, sub);
-        dbg!(&self.get()[p1 - 20..p2 + 5]);
+        // dbg!(&self.get()[p1 - 20..p2 + 5]);
+        // println!("\n{}", &self.get()[p1 - 20..p2 + 10]);
         Ok(())
     }
 
@@ -151,6 +174,7 @@ impl Content {
         let mut pos = text.find(find1).ok_or(Error::StrNotFound)?;
         pos = pos + text[pos..].find(find2).ok_or(Error::StrNotFound)? - 1;
         self.get_mut().replace_range(pos..pos + 1, &insert);
+        // eprintln!("\n{}", &self.get()[pos - 20..pos + 20]);
         Ok(())
     }
 
@@ -166,7 +190,7 @@ impl Content {
             self.insert(&content, "}", "/* Themes */");
         } else {
             if self.replace(pat, sub).is_err() {
-                self.insert(&format!("\n    --{}: {};\n", pat, sub), ":root", "}\n");
+                self.insert(&format!("\n    --{}: {};\n", pat, sub), ":root", "}\n").unwrap();
             }
         }
     }
@@ -190,8 +214,8 @@ pub static DEFAULT: &[(CssFile, Item, Value)] =
 
 lazy_static! {
     /// TODO: remove `value`
-    static ref DEFAULT_HASHMAP: HashMap<&'static Item, (&'static CssFile, &'static Value)> =
-        DEFAULT.iter().map(|(css, item, value)| (item, (css, value))).collect();
+    static ref DEFAULT_HASHMAP: HashMap<Item, CssFile> =
+        DEFAULT.iter().map(|(css, item, _)| (*item, *css)).collect();
 }
 
 static INDEX_PAGETOC: &str = r#"
@@ -199,73 +223,59 @@ static INDEX_PAGETOC: &str = r#"
 <div class="sidetoc"><nav class="pagetoc"></nav></div>
 "#;
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct Theme {
     pub cssfile: CssFile,
     pub content: Content, // ultimate str to be processed
     pub ready:   Ready,   // need a func to cover the default values according to user's config
-    pub pagetoc: bool,    // pagetoc releated
 }
 
 #[rustfmt::skip]
 impl Default for Theme {
     fn default() -> Self {
-        Self { cssfile: CssFile::Custom, content: Content::default(), 
-               ready: Ready::default(), pagetoc: false, }
+        Self { cssfile: CssFile::Custom(""), content: Content::default(), 
+               ready: Ready::default(), }
+    }
+}
+
+impl fmt::Debug for Theme {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Theme")
+         .field("cssfile", &self.cssfile)
+         .field("content", &self.content)
+         .field("ready", &self.ready.0.len())
+         .finish()
     }
 }
 
 impl Theme {
+    #[rustfmt::skip]
+    pub fn from(cssfile: CssFile, ready: Ready) -> Self {
+        Self { cssfile, ready, content: Content::default() }
+    }
+
     /// TODO: checking user's css files is not done
     /// find pagetoc and user's css files
-    pub fn process(mut self) {
-        self.cssfile(None).ready().cotent();
-        self.create_theme_file();
-    }
+    pub fn process(mut self) -> Self { self.cssfile().content().write_theme_file() }
 
-    /// TODO: `user_config` needs to search and parse css files specified by a user
-    /// and the signature finally is `user_config: Option<Path>`
-    fn cssfile(&mut self, user_config: Option<&'static str>) -> &mut Self {
-        if user_config.is_some() {
-            self.cssfile = CssFile::Custom;
+    fn cssfile(mut self) -> Self {
+        let filename = self.cssfile.filename();
+        if std::path::Path::new("theme").join(filename).exists() {
+            self.cssfile = CssFile::Custom(filename);
         }
         self
-    }
-
-    /// merge user's config
-    ///
-    /// If a user did not set `pagetoc = "true"` , `Ready` will get en **empty** default.
-    /// But he can still set something only working with pagetoc's presence,
-    /// even though that setting will *not* work (and it will lie in css files).
-    ///
-    /// More likely, a user may actually set `pagetoc = "true"` ,
-    /// then he'll get a **full** default.
-    ///
-    /// Both circumstances are **ready** to go!
-    fn ready(&mut self) -> &mut Self {
-        let mut default = if self.pagetoc { Ready::get(self.cssfile) } else { Ready::default() };
-        default.0.append(&mut self.ready.0);
-        default.0.reverse();
-        default.0.sort_by_key(|(i, _)| *i);
-        default.0.dedup_by(|(ref a, _), (ref b, _)| b.eq(a));
-        self.ready = default.0.into_iter().collect();
-        self
-    }
-
-    #[rustfmt::skip]
-    pub fn from(cssfile: CssFile, ready: Ready, pagetoc: bool) -> Self {
-        Self { cssfile, ready, pagetoc, content: Content::default() }
     }
 
     // pub fn item_value(&self, index: usize) -> Option<&(Item, Value)> { self.ready.0.get(index) }
 
     /// create a css file on demand
-    fn create_theme_file(&self) -> Result<()> {
+    fn write_theme_file(self) -> Self {
         use mdbook::utils::fs::write_file;
         write_file(std::path::Path::new("theme"),
                    self.cssfile.filename(),
-                   self.content.get().as_bytes()).map_err(|_| Error::FileNotCreated)?;
-        Ok(())
+                   self.content.get().as_bytes()).unwrap();
+        dbg!(&self);
+        self
     }
 
     /// create the dirs on demand
@@ -275,15 +285,17 @@ impl Theme {
     }
 
     /// final content to be written into `theme` dir
-    fn cotent(&mut self) -> &str {
-        dbg!(&self);
-        // empty content means not having processed the content
+    /// empty content means not having processed the content
+    fn content(mut self) -> Self {
+        // dbg!(&self);
         if self.content.get() == "" {
             // TODO: need to consider a user's file
             self.content = Content::from(self.cssfile);
+            // eprintln!("{}", self.content);
             self.content_process();
         }
-        self.content.get()
+        // dbg!(&self.ready);
+        self
     }
 
     /// process contents of different files
@@ -302,12 +314,42 @@ impl Theme {
         }
     }
 
-    /// When `pagetoc = "true"`, a bunch of files need to change;
-    /// when NOT true, do nothing.
+    /// Swich to another cssfile and process its content, which can repeat.
+    fn ready(mut self, cssfile: CssFile) -> Self {
+        self.cssfile = cssfile;
+        self.ready = Ready::get_defualt(cssfile);
+        self.process()
+    }
+
+    /// When `pagetoc = "true"`, a bunch of files need to change; when NOT true, do nothing.
     fn pagetoc(self) {
-        if self.pagetoc && self.cssfile == CssFile::Pagetoc {
-            // TODO
-            Theme::from(CssFile::Variables, Ready::get(CssFile::Variables), true);
+        if self.cssfile == CssFile::Pagetoc {
+            eprintln!("pagetoc = true");
+            self.ready(CssFile::Variables);
+            // .ready(CssFile::Index)
+            // .ready(CssFile::General)
+            // .ready(CssFile::Chrome)
         }
     }
+
+    // TODO: already delete `pagetoc` field
+    // merge user's config
+    //
+    // If a user did not set `pagetoc = "true"` , `Ready` will get en **empty** default.
+    // But he can still set something only working with pagetoc's presence,
+    // even though that setting will *not* work (and it will lie in css files).
+    //
+    // More likely, a user may actually set `pagetoc = "true"` ,
+    // then he'll get a **full** default.
+    //
+    // Both circumstances are **ready** to go!
+    // fn ready(&mut self) -> &mut Self {
+    // let mut default = if self.pagetoc { Ready::get(self.cssfile) } else { Ready::default() };
+    // default.0.append(&mut self.ready.0);
+    // default.0.reverse();
+    // default.0.sort_by_key(|(i, _)| *i);
+    // default.0.dedup_by(|(ref a, _), (ref b, _)| b.eq(a));
+    // self.ready = default.0.into_iter().collect();
+    // self
+    // }
 }
